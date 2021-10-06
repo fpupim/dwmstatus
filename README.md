e60e6d94795e7fbf587b7799dc294b489f3a3738
fpupim <fpupim@tutanota.com>
2021-10-06 20:35:38 -0300

    :^)

---
+++ README.md
@@ -0,0 +1,3 @@
+# DWMSTATUS
+Just added a few functions, changed a few things.
+Should pop up a "module" whenever you listen to music on mpd, I should probably add some images to this file.
---
+++ dwmstatus.c
@@ -0,0 +1,230 @@
+/*
+ * Copy me if you can.
+ * by 20h
+ */
+
+#define _BSD_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <mpd/client.h>
+
+#include <X11/Xlib.h>
+
+char *tzsaopaulo = "America/Sao_Paulo";
+
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
+void
+settz(char *tzname)
+{
+	setenv("TZ", tzname, 1);
+}
+
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	settz(tzname);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL)
+		return smprintf("");
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		return smprintf("");
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+readfile(char *base, char *file)
+{
+	char *path, line[513];
+	FILE *fd;
+
+	memset(line, 0, sizeof(line));
+
+	path = smprintf("%s/%s", base, file);
+	fd = fopen(path, "r");
+	free(path);
+	if (fd == NULL)
+		return NULL;
+
+	if (fgets(line, sizeof(line)-1, fd) == NULL)
+		return NULL;
+	fclose(fd);
+
+	return smprintf("%s", line);
+}
+
+char *
+getbattery(char *base)
+{
+	char *co, status;
+	int descap, remcap;
+
+	descap = -1;
+	remcap = -1;
+
+	co = readfile(base, "present");
+	if (co == NULL)
+		return smprintf("");
+	if (co[0] != '1') {
+		free(co);
+		return smprintf("not present");
+	}
+	free(co);
+
+	co = readfile(base, "charge_full_design");
+	if (co == NULL) {
+		co = readfile(base, "energy_full_design");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &descap);
+	free(co);
+
+	co = readfile(base, "charge_now");
+	if (co == NULL) {
+		co = readfile(base, "energy_now");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &remcap);
+	free(co);
+
+	co = readfile(base, "status");
+	if (!strncmp(co, "Discharging", 11)) {
+		status = '-';
+	} else if(!strncmp(co, "Charging", 8)) {
+		status = '+';
+	} else {
+		status = '?';
+	}
+
+	if (remcap < 0 || descap < 0)
+		return smprintf("invalid");
+
+	return smprintf("%.0f%%%c", ((float)remcap / (float)descap) * 100, status);
+}
+
+/* simple function to retrieve mpd status */
+char *
+getmpdstat() {
+    struct mpd_song * song = NULL;
+	const char * title = NULL;
+	const char * artist = NULL;
+	char * retstr = NULL;
+	int elapsed = 0, total = 0;
+    struct mpd_connection * conn ;
+    if (!(conn = mpd_connection_new("localhost", 0, 30000)) ||
+        mpd_connection_get_error(conn)){
+            return smprintf("Err");
+    }
+
+    mpd_command_list_begin(conn, true);
+    mpd_send_status(conn);
+    mpd_send_current_song(conn);
+    mpd_command_list_end(conn);
+
+    struct mpd_status* theStatus = mpd_recv_status(conn);
+        if ((theStatus) && (mpd_status_get_state(theStatus) == MPD_STATE_PLAY)) {
+                mpd_response_next(conn);
+                song = mpd_recv_song(conn);
+                title = smprintf("%s",mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
+                artist = smprintf("%s",mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
+
+                elapsed = mpd_status_get_elapsed_time(theStatus);
+                total = mpd_status_get_total_time(theStatus);
+                mpd_song_free(song);
+                retstr = smprintf("%s - %s: %.2d:%.2d/%.2d:%.2d | ",
+                                artist, title,
+				elapsed/60, elapsed%60,
+				total/60, total%60);
+                free((char*)title);
+                free((char*)artist);
+        }
+        else retstr = smprintf("");
+		mpd_response_finish(conn);
+		mpd_connection_free(conn);
+		return retstr;
+}
+
+int
+main(void)
+{
+	char *status;
+	char *bat;
+	char *tmsp;
+	char *mpd;
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "dwmstatus: cannot open display.\n");
+		return 1;
+	}
+
+	for (;;sleep(2)) {
+		mpd = getmpdstat();
+		bat = getbattery("/sys/class/power_supply/BAT0");
+		tmsp = mktimes("H: %H:%M | D: %d/%m/%y", tzsaopaulo);
+
+		status = smprintf("%sB: %s | %s",
+				mpd, bat, tmsp
+				);
+		setstatus(status);
+
+		free(mpd);
+		free(bat);
+		free(tmsp);
+		free(status);
+	}
+
+	XCloseDisplay(dpy);
+
+	return 0;
+}
+
